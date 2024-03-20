b78531acd487168dacab3293039dff54980953bf
nguyenphamtuanngoc1003 <nguyenphamtuanngoc1003@gmail.com>
2024-03-20 17:27:03 +0700

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from './component/Screen/lab3 (1)/Main';
+import Bai2 from './component/Screen/lab1 (1)/bai2';
+import Bai3Lab1 from './component/Screen/lab1 (1)/bai3';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <Main></Main>
+    // <Bai2></Bai2>
+    // <Bai3Lab1></Bai3Lab1>
+  )
 }
 
 const styles = StyleSheet.create({
